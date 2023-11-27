1d1410bf37b3571492d789cde1f09644dba46f0a
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2023-11-27 08:27:54 -0300

    ok

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -3,7 +3,6 @@ import { HOMEPAGE_QUERY } from '../../../interfaces/queries'
 import { performRequest } from '../../../lib/datocms'
 import Renderer from '../../../components/renderer/renderer'
 import { Metadata } from 'next'
-import { HOMEPAGE_QUERY } from '../../../interfaces/queries'
 
 export const metadata: Metadata = {
   title: 'Post | MundoDev'
@@ -19,7 +18,9 @@ export default async function Post({ params }: { params: { slug: string } }) {
   const { allArticles } = await performRequest({
     query: HOMEPAGE_QUERY,
     revalidate: 10
-  }
+  })
+  
   const articles = allArticles
+
   return <Renderer post={post.article} allPosts={articles} />
 }
